16686ebea11b598b72b8ec371b8ca5adf1d6417f
joaomarcos <joaomarcos@laptop>
2016-12-07 20:43:37 -0300

    Inicial

---
+++ src/test/java/io/github/joaomarccos/testecommockspring/tomcat/SprintIntegrationTest.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.joaomarccos.testecommockspring.tomcat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+/**
+ *
+ * @author joaomarcos
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SampleTomcatApplication.class)
+@WebAppConfiguration
+public class SprintIntegrationTest {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context).build();
+    }
+    
+    @Test
+    public void test() throws Exception{
+        mvc.perform(get("/")).andExpect(status().isOk());
+    }
+
+}
